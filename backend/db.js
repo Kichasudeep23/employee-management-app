839dd04e1b01c266a431962dbb5597d219b304ac
Sudeep H <sudeep@Sudeeps-MacBook-Air.local>
2026-04-02 11:48:03 +0530

    Update backend for Render database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "db",
-  database: "employee_db",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
